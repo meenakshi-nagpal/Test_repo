5728c3db892c2c0dab3e2a1bbe37c121321c2ca2
meenakshi-nagpal <meenakshi.nagpal22@@gmail.com>
2018-06-05 14:54:27 -0400

    deleteing

--- app/security/Access.java
+++
@@ -1,8 +0,0 @@
-package security;
-
-public interface Access {
-	
-	public boolean isAuthenticated(String standardid, String password);
-
-	public boolean isAuthorized(String standardid, String resource, String role);
-}
